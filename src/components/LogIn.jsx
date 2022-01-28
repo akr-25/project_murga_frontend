44bc4dd56262aed8add4cd1f4632007a6528ef21
Eeshaan Dutta <esnd18@gmail.com>
2022-01-28 23:36:57 +0530

    frontendCleanUp

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom"; 
-import axios from 'axios';
 
 export default function SignUp() {
   const [username, setUsername] = useState('');
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState, useEffect } from 'react'; 
-import axios from 'axios';
 
 export default function SignUp() {
   const [username, setUsername] = useState();
