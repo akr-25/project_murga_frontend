9d19beeafb7d35d1c52a34d4605a450a47f49c50
Eeshaan Dutta <esnd18@gmail.com>
2022-06-15 11:00:42 +0530

    (NOT WORKING) logout

--- src/pages/LogOut.jsx
+++ src/pages/LogOut.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
+import { useNavigate } from "react-router-dom";
 
 export default function LogOut() {
     const { auth } = useAuth(); 
+    const navigate = useNavigate(); 
 
     function logOut(event){
         event.preventDefault();
-        window.open("http://localhost:3001/auth/logout", "_self")
+        try{
+            fetch("http://localhost:3001/auth/logout", {
+                credentials: 'include', 
+                redirect: 'follow', 
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+            })
+            .then(res => res.json())
+            .then(data => navigate("/"))
+            .catch(err => console.log(err))
+        }
+        catch(err){
+            console.log(err); 
+        }
+        
+
+        // event.preventDefault();
+        // window.open("http://localhost:3001/auth/logout", "_self")
     }
     
     return <div>
